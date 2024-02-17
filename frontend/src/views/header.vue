1a7f433b4a1064dd73dc2382cd0c7ae8d556d101
kishan <kumar.kishankumar.kishan38@gmail.com>
2024-02-17 16:03:58 +0530

    Added logo

--- frontend/src/views/header.vue
+++ frontend/src/views/header.vue
@@ -34,7 +34,7 @@ const user = {
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
           <div class="flex-shrink-0">
-            <img class="h-8 w-8" src="/Logo.svg" alt="Your Company" />
+            <img class="" src="/MMD_logo.png" alt="Your Company" />
           </div>
           <!-- <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
